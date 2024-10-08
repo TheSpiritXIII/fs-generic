5e777d9106be175111fd495557b97d8a887a80c1
Daniel Hrabovcak <thespiritxiii@gmail.com>
2024-10-07 22:46:08 -0400

    feat: add regen-src tool

--- regen-src/src/main.rs
+++ regen-src/src/main.rs
@@ -1,20 +1,204 @@
 #![warn(clippy::pedantic)]
+use std::fs;
 use std::fs::OpenOptions;
+use std::io;
 use std::io::Write;
 use std::path;
 
 use log::info;
+use thiserror::Error;
 
 const HEADER: &str = include_str!("../data/header.rs");
 
 fn main() -> anyhow::Result<()> {
 	env_logger::init();
 
+	let input_path = path::absolute("data/std.json")?;
+	info!("Parsing doc from {}...", input_path.display());
+	let input_data = fs::read_to_string(input_path).unwrap();
+	let doc_crate = serde_json::from_str(&input_data)?;
+
 	let output_path = path::absolute("src/generated.rs")?;
 	info!("Regenerating source into {}...", output_path.display());
 	let mut out_file =
 		OpenOptions::new().write(true).create(true).truncate(true).open(output_path)?;
-	write!(out_file, "{HEADER}")?;
+	writeln!(out_file, "{HEADER}")?;
+	json_to_rs(&doc_crate, &mut out_file)?;
 	info!("Done!");
+	Ok(())
+}
+
+#[derive(Error, Debug)]
+pub enum SourceError {
+	#[error("unable to write source")]
+	Io(#[from] io::Error),
+	#[error("parse error: {0}")]
+	ParseError(&'static str),
+}
+
+fn json_to_rs<W: Write>(doc_crate: &rustdoc_types::Crate, out: &mut W) -> Result<(), SourceError> {
+	let root_module = doc_crate.index.get(&doc_crate.root).unwrap();
+	let rustdoc_types::ItemEnum::Module(doc_module) = &root_module.inner else {
+		return Err(SourceError::ParseError("expected root to be a module"));
+	};
+	for id in &doc_module.items {
+		if let Some(item) = doc_crate.index.get(id) {
+			if let rustdoc_types::ItemEnum::Module(item_module) = &item.inner {
+				if item.name.as_ref().is_some_and(|name| name == "fs") {
+					for id in &item_module.items {
+						let item = doc_crate.index.get(id).unwrap();
+						if let rustdoc_types::ItemEnum::Function(function) = &item.inner {
+							print_function(out, doc_crate, item, function)?;
+						}
+					}
+				}
+			}
+		}
+	}
+	Ok(())
+}
+
+fn print_function<W: Write>(
+	out: &mut W,
+	doc_crate: &rustdoc_types::Crate,
+	item: &rustdoc_types::Item,
+	function: &rustdoc_types::Function,
+) -> io::Result<()> {
+	if let Some(docs) = &item.docs {
+		for line in docs.lines() {
+			writeln!(out, "/// {line}")?;
+		}
+	}
+	write!(out, "pub fn {}", item.name.as_ref().unwrap())?;
+	if !function.generics.params.is_empty() {
+		write!(out, "<")?;
+		for generic_param in &function.generics.params {
+			write!(out, "{}: ", generic_param.name)?;
+			match &generic_param.kind {
+				rustdoc_types::GenericParamDefKind::Type {
+					bounds,
+					default,
+					synthetic,
+				} => {
+					for bound in bounds {
+						match bound {
+							rustdoc_types::GenericBound::TraitBound {
+								trait_,
+								generic_params,
+								modifier,
+							} => {
+								if !generic_params.is_empty()
+									|| *modifier != rustdoc_types::TraitBoundModifier::None
+								{
+									unimplemented!();
+								}
+								write!(out, "{}", trait_.name)?;
+								if let Some(args) = &trait_.args {
+									print_generic_arg(out, doc_crate, args)?;
+								}
+								write!(out, " + ")?;
+							}
+							_ => todo!(),
+						}
+					}
+					if *synthetic || default.is_some() {
+						unimplemented!();
+					}
+				}
+				_ => unimplemented!(),
+			}
+			// rustdoc_pretty_type(out, &doc_crate, &generic_param.kind)?;
+			write!(out, ", ")?;
+		}
+		write!(out, ">")?;
+	}
+	write!(out, "(")?;
+	for (input_name, input_type) in &function.decl.inputs {
+		write!(out, "{input_name}: ")?;
+		print_type(out, doc_crate, input_type)?;
+		write!(out, ", ")?;
+	}
+	write!(out, ")")?;
+	if let Some(output_type) = &function.decl.output {
+		write!(out, " -> ")?;
+		print_type(out, doc_crate, output_type)?;
+	}
+	writeln!(out, " {{")?;
+	write!(out, "	std::fs::{}(", item.name.as_ref().unwrap())?;
+	for (input_name, _) in &function.decl.inputs {
+		write!(out, "{input_name}, ")?;
+	}
+	writeln!(out, ")")?;
+	writeln!(out, "}}")?;
+	Ok(())
+}
+
+fn print_generic_arg<W: Write>(
+	out: &mut W,
+	doc_crate: &rustdoc_types::Crate,
+	arg: &rustdoc_types::GenericArgs,
+) -> io::Result<()> {
+	if let rustdoc_types::GenericArgs::AngleBracketed {
+		args,
+		bindings,
+	} = arg
+	{
+		write!(out, "<")?;
+		for arg in args {
+			match arg {
+				rustdoc_types::GenericArg::Type(generic_type) => {
+					print_type(out, doc_crate, generic_type)?;
+				}
+				_ => unimplemented!(),
+			}
+			write!(out, ",")?;
+		}
+		write!(out, ">")?;
+		if !bindings.is_empty() {
+			unimplemented!();
+		}
+	} else {
+		unimplemented!()
+	}
+	Ok(())
+}
+
+fn print_type<W: Write>(
+	out: &mut W,
+	rustdoc_crate: &rustdoc_types::Crate,
+	rustdoc_type: &rustdoc_types::Type,
+) -> io::Result<()> {
+	match rustdoc_type {
+		rustdoc_types::Type::ResolvedPath(path) => {
+			const CRATE_PATH: &str = "crate::";
+			if path.name.starts_with(CRATE_PATH) {
+				write!(out, "std::{}", &path.name[CRATE_PATH.len()..])?;
+			} else {
+				write!(out, "{}", path.name)?;
+			}
+			if let Some(args) = &path.args {
+				print_generic_arg(out, rustdoc_crate, args)?;
+			}
+		}
+		rustdoc_types::Type::Generic(doc_generic) => {
+			write!(out, "{doc_generic}")?;
+		}
+		rustdoc_types::Type::Primitive(doc_primitive) => {
+			write!(out, "{doc_primitive}")?;
+		}
+		rustdoc_types::Type::Tuple(doc_tuple) => {
+			write!(out, "(")?;
+			for doc_tuple in doc_tuple {
+				print_type(out, rustdoc_crate, doc_tuple)?;
+			}
+			write!(out, ")")?;
+		}
+		rustdoc_types::Type::Slice(doc_slice) => {
+			write!(out, "[")?;
+			print_type(out, rustdoc_crate, doc_slice)?;
+			write!(out, "]")?;
+		}
+		_ => unimplemented!(),
+	}
 	Ok(())
 }
